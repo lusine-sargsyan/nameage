dbc18882c60b57e53e3caa967e697bbbda0e8f43
lusine <lusine.sargsyan71@outlook.com>
2019-09-13 00:11:01 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace nameage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int age;
+          string s;
+          Console.WriteLine("your name");
+          
+
+          
+         s=Console.ReadLine();
+            
+         Console.WriteLine("your age");
+         
+         age=Int32.Parse(Console.ReadLine());
+         
+         Console.WriteLine($"your name {s} your birth {2019-age}");
+
+         
+                  }
+    }
+}
